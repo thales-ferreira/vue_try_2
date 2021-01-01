dbf0a37d07cd51414483c30679b394fa57d3c4cd
Thales Ferreira <thales.ferreira@ryerson.ca>
2021-01-01 18:02:39 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="center">
+    <vs-button :active="active == 0" @click="active = 0">Active</vs-button>
+    <vs-button :active="active == 1" @click="active = 1">Default</vs-button>
+    <vs-button disabled>Disabled</vs-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    active: 0
+  })
+};
+</script>
+
+<style>
+.center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+}
+</style>
