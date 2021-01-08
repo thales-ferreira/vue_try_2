4d5c8f3b7e7a2f10f4945d5251ada9436bada829
Thales Ferreira <thales.ferreira@ryerson.ca>
2021-01-08 14:51:16 -0500

    Change in  theme colors vuesax tutorial

--- src/main.js
+++ src/main.js
@@ -3,9 +3,17 @@ import App from "./App.vue";
 import Vuesax from "vuesax";
 import "vuesax/dist/vuesax.css";
 
-Vue.use(Vuesax);
+Vue.use(Vuesax, {
+  colors: {
+    primary: "#5b3cc4",
+    success: "rgb(23, 201, 100)",
+    danger: "rgb(242, 19, 93)",
+    warning: "rgb(255, 130, 0)",
+    dark: "rgb(36, 33, 69)"
+  }
+});
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount("#app");
