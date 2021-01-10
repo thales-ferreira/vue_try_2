b901e8935f2f052f4571f45db0591bc81eecd54a
Thales Ferreira <thales.ferreira@ryerson.ca>
2021-01-10 11:11:28 -0500

    initial commit

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ Vue.use(Vuesax, {
     primary: "#5b3cc4",
     success: "rgb(23, 200, 100)",
     danger: "rgb(242, 19, 93)",
-    warning: "rgb(255, 130, 0)",
+    warning: "rgb(255, 131, 0)",
     dark: "rgb(36, 33, 69)"
   }
 });
