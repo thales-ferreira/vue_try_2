90c4a1b643c1e8f01d8a8bd8697efc49e8a2db01
Thales Ferreira <thales.ferreira@ryerson.ca>
2021-01-10 11:08:39 -0500

    Little change.

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import "vuesax/dist/vuesax.css";
 Vue.use(Vuesax, {
   colors: {
     primary: "#5b3cc4",
-    success: "rgb(23, 201, 100)",
+    success: "rgb(23, 200, 100)",
     danger: "rgb(242, 19, 93)",
     warning: "rgb(255, 130, 0)",
     dark: "rgb(36, 33, 69)"
